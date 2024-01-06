7fe35bb87412188a70385b67543cc6fcdc0dda87
Dimoiu Bogdan <bogdan.dimoiu@itperspectives.ro>
2024-01-06 02:43:38 +0200

    Netlify #2

--- api.js
+++ api.js
@@ -609,7 +609,7 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-app.use("/.netlify/functions/api", router);
+app.use("/api", router);
 
 app.listen(8080, () => {
   console.log("Listening on port 8080");
