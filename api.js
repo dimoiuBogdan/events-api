efd51809dcc70d9a5a0f205e81e9bb3415b48714
Dimoiu Bogdan <bogdan.dimoiu@itperspectives.ro>
2024-01-07 17:58:35 +0200

    update

--- api.js
+++ api.js
@@ -24,7 +24,7 @@ const database = mysql
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || undefined,
     ssl: {
       rejectUnauthorized: false,
     },
